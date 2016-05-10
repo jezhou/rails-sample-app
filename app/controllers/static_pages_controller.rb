9169fcc3e462da08f609739d113896407aec936d
Jesse Zhou <jesse.cy.zhou@gmail.com>
2016-05-10 04:31:32 -0700

    Added static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
+
+  # In rails, views will auto render at the end of a function.
   def home
   end
 
   def help
+  end
+
+  def about
   end
 end
